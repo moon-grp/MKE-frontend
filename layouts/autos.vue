5a5290415077f9b218ce1c4900e52d6235c78cde
emohex <olumidemm@gmail.com>
2021-03-09 16:25:47 +0100

    autos page

---
+++ layouts/autos.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app dark>
+    <div>
+      <v-main>
+        <v-container>
+          <nuxt />
+        </v-container>
+      </v-main>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Loading from '~/components/loading.vue'
+
+export default {
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background: #ffffff;
+}
+a {
+  text-decoration: none;
+}
+
+.fnt {
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  color: #ecf4ff;
+}
+</style>
---
+++ pages/autos/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-row class="justify-center">
+      <div class="mx-auto">
+        <Lottie
+          :width="600"
+          :height="500"
+          :options="lottieOptions"
+          v-on:animCreated="handleAnimation"
+        />
+      </div>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Lottie from 'vue-lottie/src/lottie.vue'
+import * as animationData from '~/assets/uc.json'
+
+export default {
+  layout: 'autos',
+  components: {
+    Lottie,
+  },
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: animationData.default },
+    }
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim
+    },
+  },
+}
+</script>
