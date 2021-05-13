ac5d4f9e0a22cf680de4eaccd68528fcf561f8b6
emohex <olumidemm@gmail.com>
2021-05-13 09:38:20 +0100

    fixed admin pannel

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <v-row class="mt-4">
-      <v-col cols="3" v-for="item in getProducts" :key="item._id.$oid">
+      <v-col
+        sm="4"
+        xl="2"
+        lg="3"
+        md="3"
+        xs="6"
+        v-for="item in getProducts"
+        :key="item._id.$oid"
+      >
         <v-card max-height="500">
           <v-img
             :src="item.imgUrl"
